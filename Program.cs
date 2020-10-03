f9c2f8dc84d84955e4f852e31c59bc60557c7023
Jacob Kohl <jkohl7@my.wctc.edu>
2020-10-03 10:49:50 -0500

    2D array and Stream Reader

--- Program.cs
+++ Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.IO;
+using System.Collections;
 
 namespace TIcketObject
 {
@@ -6,7 +8,20 @@ namespace TIcketObject
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //create array and read file
+            ArrayList TDArray = new ArrayList();
+            string file = "tickets.csv";
+            StreamReader sr = new StreamReader(file);
+           
+            while (!sr.EndOfStream)
+            {
+                string line = sr.ReadLine();
+                string[] arr = line.Split(",");
+                TDArray.Add(arr);
+            }
+            
+
+            
         }
     }
 }
