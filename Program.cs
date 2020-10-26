bba4734ae2035ca7ba17f542355be1f7c42c84cd
Jacob Kohl <jkohl7@my.wctc.edu>
2020-10-25 21:47:32 -0500

    finished task option

--- Program.cs
+++ Program.cs
@@ -103,23 +103,23 @@ namespace TIcketObject
             //Promt the user 
             Console.WriteLine("Enter TicketID");
             string id = Console.ReadLine();
-            Console.WriteLine("Enter a summary of the ticket");
+            Console.WriteLine("Enter a summary of the enhancement");
             string summ = Console.ReadLine();
-            Console.WriteLine("Enter the status of the ticket");
+            Console.WriteLine("Enter the status of the enhancement");
             string status = Console.ReadLine();
-            Console.WriteLine("Enter the priority of the ticket");
+            Console.WriteLine("Enter the priority of the enhancement");
             string prio = Console.ReadLine();
-            Console.WriteLine("Enter the submiter of the ticket");
+            Console.WriteLine("Enter the submiter of the enhancement");
             string submit = Console.ReadLine();
-            Console.WriteLine("Enter the assigner of the ticket");
+            Console.WriteLine("Enter the assigner of the enhancement");
             string assn = Console.ReadLine();
-            Console.WriteLine("Enter the software for the ticket");
+            Console.WriteLine("Enter the software for the enhancement");
             string soft = Console.ReadLine();
-            Console.WriteLine("Enter the Cost for the ticket");
+            Console.WriteLine("Enter the Cost for the enhancement");
             string cost = Console.ReadLine();
-            Console.WriteLine("Enter the Reason for the ticket");
+            Console.WriteLine("Enter the Reason for the enhancement");
             string reason = Console.ReadLine();
-            Console.WriteLine("Enter the Estimate of the ticket");
+            Console.WriteLine("Enter the Estimate of the enhancement");
             string est = Console.ReadLine();
 
 
@@ -157,7 +157,69 @@ namespace TIcketObject
 
         }else if (choice == "3")
         {
-            Console.WriteLine("This is 3");
+            ArrayList TDArray = new ArrayList();
+            string file = "Task.csv";
+            StreamReader sr = new StreamReader(file);
+           
+            while (!sr.EndOfStream)
+            {
+                string line = sr.ReadLine();
+                string[] arr = line.Split(",");
+                TDArray.Add(arr);
+            }
+            sr.Close();
+            StreamWriter sw = new StreamWriter(file);
+
+            //Promt the user 
+            Console.WriteLine("Enter TicketID");
+            string id = Console.ReadLine();
+            Console.WriteLine("Enter a summary of the task");
+            string summ = Console.ReadLine();
+            Console.WriteLine("Enter the status of the task");
+            string status = Console.ReadLine();
+            Console.WriteLine("Enter the priority of the task");
+            string prio = Console.ReadLine();
+            Console.WriteLine("Enter the submiter of the task");
+            string submit = Console.ReadLine();
+            Console.WriteLine("Enter the assigner of the task");
+            string assn = Console.ReadLine();
+            Console.WriteLine("Enter the project name for the task");
+            string projectname = Console.ReadLine();
+            Console.WriteLine("Enter the due date for the task");
+            string duedate = Console.ReadLine();
+    
+
+            //Asks and gets data for multiple users
+            Console.WriteLine("Is anyone watching the ticket? (Y/N)");
+            string reply = Console.ReadLine().ToUpper();
+
+            bool whosWatching = true;
+            if (reply.Equals("N")) whosWatching = false;
+            string watch = "";
+            while (whosWatching)
+            {
+                Console.WriteLine("Who is watching?");
+                watch += Console.ReadLine();
+                watch += "|";
+                Console.WriteLine("Is there anyone else? (Y/N)");
+                reply = Console.ReadLine().ToUpper();
+                if (reply.Equals("N")) whosWatching = false;
+            }
+            String[] userInput = new string[] { id, summ, status, prio, submit, assn, projectname, duedate, watch};
+              TDArray.Add(userInput);
+
+
+           foreach (string[] l in TDArray)
+            {
+                for (int x = 0; x < 9; x++)
+                {
+                    sw.Write(l[x] + ",");
+
+                }
+                sw.WriteLine();
+
+            }
+            sw.Close();
         }
 
          } while (choice == "1" || choice == "2" || choice =="3");
